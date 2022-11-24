84d3eb441b59c3ae06311c8b6d7c654b6a8bf3fe
Mikhail <Popov.Mikhail.A@gmail.com>
2022-11-25 02:15:01 +0600

    Lesson 03

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,5 +1,5 @@
 ﻿// 25. Вывести на экран кубы чисел от 1 до N
-System.Console.WriteLine("Введите число N:");
+/*System.Console.WriteLine("Введите число N:");
 int N=Convert.ToInt32(Console.ReadLine());
 int i=1;
 while(i<=N)
@@ -7,21 +7,19 @@ while(i<=N)
     System.Console.Write($"{i*i*i} ");
     i=i+1;
 }
-
-
-/*
-int Cube(int i)
-{
-    cube=0;
-    i=1;
-     while(i<=N)
-      {
-         cube=i*i*i;
-         i=i+1;
-      }
-    return cube;
-}
-int N=Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите число N:");
-System.Console.WriteLine(Cube(N));
 */
+
+
+int Cube(int N)
+{
+    int i=1, p=1;
+    while(i<=N)
+    {
+      p = i*i*i;
+      i=i+1;
+    }
+    return p;
+}
+System.Console.Write("Введите число n: ");
+int n=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Cube(n));
---
+++ 30/Program.cs
@@ -0,0 +1,15 @@
+﻿// 30. Написать программу вычисления произведения чисел от 1 до N
+
+int Cube(int N)
+{
+    int p=1, j=0;
+    while(j<N)
+    {
+      p = p*(j+1);
+      j=j+1;
+    }
+    return p;
+}
+System.Console.Write("Введите число => 1 : ");
+int n=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Cube(n));
