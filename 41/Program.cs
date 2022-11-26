35685e46b92dc4cad694c5c562f662b5731c4df1
Mikhail <Popov.Mikhail.A@gmail.com>
2022-11-27 02:29:37 +0600

    Lesson 04

---
+++ 41/Program.cs
@@ -0,0 +1,13 @@
+﻿// 41. Задать массив из 8 элементов, заполненных нулями и единицами
+// вывести их на экран
+
+//int[] nums = new int[8] { 0, 1, 0, 1, 0, 1, 0, 1 };
+//Array.ForEach(nums,Console.Write );
+
+
+void PrintMessage(string message)
+{
+    Console.Write(message);
+}
+PrintMessage("0, 1, 0, 1, 0, 1, 0, 1 ");
+
