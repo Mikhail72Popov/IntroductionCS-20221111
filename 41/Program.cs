b5d39cffc975ed069db034ddec491aded87e9420
Mikhail <Popov.Mikhail.A@gmail.com>
2022-11-27 18:49:54 +0600

    Lesson 04

--- 41/Program.cs
+++ 41/Program.cs
@@ -1,13 +1,29 @@
 ﻿// 41. Задать массив из 8 элементов, заполненных нулями и единицами
 // вывести их на экран
 
-//int[] nums = new int[8] { 0, 1, 0, 1, 0, 1, 0, 1 };
-//Array.ForEach(nums,Console.Write );
-
-
+int[] RandomIntArray(int size=8)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(0,1);
+    return a;
+}
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+int[] m=RandomIntArray(8);
+Print(m);
+/*
+int[] nums = new int[8] { 0, 1, 0, 1, 0, 1, 0, 1 };
+Array.ForEach(nums,Console.Write );
+*/
+/*
 void PrintMessage(string message)
 {
     Console.Write(message);
 }
 PrintMessage("0, 1, 0, 1, 0, 1, 0, 1 ");
-
+*/
