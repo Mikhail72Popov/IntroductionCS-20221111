d9e8b0a46de8d10c773f855a4324e902959164fe
Mikhail <Popov.Mikhail.A@gmail.com>
2022-12-05 23:06:51 +0600

    Lesson 04

--- 41/Program.cs
+++ 41/Program.cs
@@ -6,13 +6,14 @@ int[] RandomIntArray(int size=8)
     int[] a=new int[size];
     Random random=new Random();
     for(int i=0;i<size;i++)
-        a[i]=random.Next(0,1);
+        a[i]=random.Next(0,2);
     return a;
 }
 void Print(int[] a)
 {
     for(int i=0;i<a.Length;i++)
         System.Console.Write($"{a[i],5}");
+        System.Console.WriteLine();
 }
 int[] m=RandomIntArray(8);
 Print(m);
